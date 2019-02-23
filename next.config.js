747380c040272c6096628595b9bfd17c69cd600a
kristoffer petersen <krizzmp@gmail.com>
2019-02-23 13:40:25 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,30 +1,14 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === "development"
-    ? {}
-    : require("next-server/constants");
+const withCss = require("@zeit/next-css");
+const withTypescript = require("@zeit/next-typescript");
+const withMDX = require("@zeit/next-mdx")({
+  extension: /.mdx?$/
+});
 
-const config = {
-};
-
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    return config;
-  }
-  const withPlugins = require("next-compose-plugins");
-  const withTypescript = require("@zeit/next-typescript");
-  const withCss = require("@zeit/next-css");
-  const withMDX = require("@zeit/next-mdx")();
-  return withPlugins(
-    [
-      [withTypescript],
-      [
-        withMDX,
-        {
-          pageExtensions: ["js", "tsx", "jsx", "mdx"]
-        }
-      ],
-      [withCss, { cssModules: true }]
-    ],
-    config
-  );
-};
+module.exports = withTypescript(
+  withMDX(
+    withCss({
+      cssModules: true,
+    pageExtensions: ["js", "jsx", "ts", "tsx"],
+    target: "serverless",
+  }))
+);
