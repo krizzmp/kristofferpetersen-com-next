fe1f2337b105df336549b4a232a80d09bfbe257a
kristoffer petersen <krizzmp@gmail.com>
2019-02-23 23:44:39 +0100

    add links the index page and style them

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import Head from "next/head";
 import Link from "next/link";
 import { Header } from "../components/header";
-
+// @ts-ignore
+import t from "./main.css";
 const Index = () => (
   <div>
     <Head>
       <title>kristoffer petersen</title>
-
     </Head>
-    <Header/>
+    <Header />
     <Link href="/portfolio">
-      <a>portfoilo</a>
+      <a>
+        <div className={t.card}>portfoilo</div>
+      </a>
+    </Link>
+    <Link href="https://hatch.now.sh">
+      <a>
+        <div className={t.card}>hatch</div>
+      </a>
     </Link>
   </div>
 );
