bbd89435345c82ad92862424f6f9e12839b0050b
kristoffer petersen <krizzmp@gmail.com>
2019-02-23 21:11:08 +0100

    back to a single style

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,19 +1,13 @@
 import Head from "next/head";
 import { Header } from "../components/header";
 // @ts-ignore
-import s from "./portfolio.css";
-// @ts-ignore
-import s2 from "./portfolio_kindle.css";
-// @ts-ignore
-import s3 from "./portfolio_kindle2.css";
+import c from "./portfolio.css";
 import "./main.css";
 // @ts-ignore
 import Document, { meta } from "./test.mdx";
-import { useState } from "react";
 const { date, author, title } = meta;
 
 function Index() {
-  let [c, setTheme] = useState(s3);
   return (
     <div>
       <Head>
@@ -21,9 +15,6 @@ function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Header title={"portfolio"} />
-      <button onClick={() => setTheme(s)}>medium</button>
-      <button onClick={() => setTheme(s2)}>kindle</button>
-      <button onClick={() => setTheme(s3)}>iBooks</button>
       <div className={c.portfolio}>
         <article>
           <h2 className={c.title}>{title}</h2>
