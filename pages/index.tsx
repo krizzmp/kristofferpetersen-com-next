f5bad74925e1a7a1b7df13ffe094ae0c66fb91a2
kristoffer petersen <krizzmp@gmail.com>
2019-02-24 17:21:40 +0100

    update grid and change link boxes into columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,22 +3,50 @@ import Link from "next/link";
 import { Header } from "../components/header";
 // @ts-ignore
 import t from "./main.css";
+function Grid({ children }: any) {
+  return (
+    <div
+      className={t.grid}
+    >
+      {children}
+    </div>
+  );
+}
 const Index = () => (
   <div>
     <Head>
       <title>kristoffer petersen</title>
     </Head>
     <Header />
-    <Link href="/portfolio">
-      <a>
-        <div className={t.card}>portfoilo</div>
-      </a>
-    </Link>
-    <Link href="https://hatch.now.sh">
-      <a>
-        <div className={t.card}>hatch</div>
-      </a>
-    </Link>
+
+    <Grid maxWidth={700} gutter={32} columnCout={3}>
+      <div className={t.t}>
+        <h2>
+          <Link href="/portfolio">
+            <a>
+              <div>Portfoilo</div>
+            </a>
+          </Link>
+        </h2>
+        <p>
+          Check out my blog about considerations i make about event sourcing and
+          functional programming in the context of a school project.
+        </p>
+      </div>
+      <div className={t.t}>
+        <h2>
+          <Link href="https://hatch.now.sh">
+            <a>
+              <div>Hatch</div>
+            </a>
+          </Link>
+        </h2>
+        <p>
+          Hatch is a collaborative diagramming tool that i created in my spare
+          time.
+        </p>
+      </div>
+    </Grid>
   </div>
 );
 
