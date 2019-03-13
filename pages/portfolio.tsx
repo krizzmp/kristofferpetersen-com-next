a380494d17118d941b093e6bf7542d3de0d4e09c
kristoffer petersen <krizzmp@gmail.com>
2019-03-13 06:52:03 +0100

    post 2

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -2,24 +2,33 @@ import { Header } from "../components/header";
 // @ts-ignore
 import c from "./portfolio.css";
 import "./main.css";
+
 // @ts-ignore
-import Document, { meta } from "./test.mdx";
-const { date, author, title } = meta;
+import * as t from "./test.mdx";
+// @ts-ignore
+import * as g from "./post2.mdx";
+
+function Post(p: { doc: any; meta: { date: any; author: any; title: any } }) {
+  return (
+    <article>
+      <h2 className={c.title}>{p.meta.title}</h2>
+      <div className={c.date}>
+        {p.meta.date} by {p.meta.author}
+      </div>
+      <div className={c.content}>
+        <p.doc />
+      </div>
+    </article>
+  );
+}
 
 function Index() {
   return (
     <div>
       <Header title={"portfolio"} />
       <div className={c.portfolio}>
-        <article>
-          <h2 className={c.title}>{title}</h2>
-          <div className={c.date}>
-            {date} by {author}
-          </div>
-          <div className={c.content}>
-            <Document />
-          </div>
-        </article>
+        <Post doc={g.default} meta={g.meta} />
+        <Post doc={t.default} meta={t.meta} />
       </div>
     </div>
   );
